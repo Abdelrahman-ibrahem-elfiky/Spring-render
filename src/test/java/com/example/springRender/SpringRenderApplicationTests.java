d90ce1c85c71873949f3b2f4e0907f27807f459c
Abdelrahman-ibrahem-elfiky <119974507+Abdelrahman-ibrahem-elfiky@users.noreply.github.com>
2024-03-06 10:06:57 +0200

    Add files via upload

---
+++ src/test/java/com/example/springRender/SpringRenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springRender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
